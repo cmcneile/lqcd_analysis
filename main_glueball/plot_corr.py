e48e50cf1ce9c5cd0439b2d55ca0fa1f297fe27f
Craig McNeile <craigmcneile@PM6-0656.local>
2016-07-15 16:04:30 +0100

    The plot code now almost works

--- main_glueball/plot_corr.py
+++ main_glueball/plot_corr.py
@@ -9,10 +9,14 @@ import pickle
 
 print "Starting to read the file "
 
-with open('data.pickle', 'rb') as f:
+with open('data/eigstore.pickle', 'rb') as f:
 # The protocol version used is detected automatically, so we do not
 # have to specify it.
   eigstore = pickle.load(f)
+
+with open('data/tvalues.pickle', 'rb') as f:
+     tvalues = pickle.load(f)
+
 
 
 print "The data has been read"
@@ -27,12 +31,31 @@ print "Summary"
 for ieig in range(0,maxeig):
    print "Eigenvalue = " , ieig
    for tp in range(0,tval):
-      print tp , eigstore[ieig,0,tp] , eigstore[ieig,1,tp] 
+      print tvalues[tp] , eigstore[ieig,0,tp] , eigstore[ieig,1,tp] 
 
 
 ##from matplotlib import pylab
 ##from matplotlib.pyplot import *
-from matplotlib import *
+##from matplotlib import *
+
+import pylab
+##from matplotlib.pyplot import *
+
+pylab.ylim(ymax=1) # adjust the max leaving min unchanged
+pylab.ylim(ymin=-0.5) # adjust the min leaving max unchanged
+
+pylab.xlim(xmin=0) # adjust the min leaving max unchanged
+pylab.xlim(xmax=7) # adjust the max leaving min unchanged
+
+pylab.title('Glueball 0RPmR ', {'color':'k','fontsize':18})
+
+pylab.ylabel('<G(t)G(t+1)> ', {'color':'k','fontsize':18})
+pylab.xlabel('t', {'color':'k','fontsize':18})
+
+pylab.errorbar(x=tvalues, y=eigstore[ieig,0,0:tval],yerr=eigstore[ieig,1,0:tval],fmt='bs' ,linewidth=2  )
+pylab.savefig('EigMass.pdf',format="pdf")
+pylab.show()
+
 
       
 sys.exit(0)
--- main_glueball/setup.sh
+++ main_glueball/setup.sh
@@ -1,6 +1,8 @@
+top=/Users/craigmcneile/projects/openbound_glue/eigen/lqcd_analysis
+##top=/home/cmcneile/Gcorr_case3_new_rago/py_lqcd_analysis
 
-export PYTHONPATH=${PYTHONPATH}:/home/cmcneile/Gcorr_case3_new_rago/py_lqcd_analysis/lib_analysis
-export PYTHONPATH=${PYTHONPATH}:/home/cmcneile/Gcorr_case3_new_rago/py_lqcd_analysis/lib_glueball
+export PYTHONPATH=${PYTHONPATH}:${top}/lib_analysis
+export PYTHONPATH=${PYTHONPATH}:${top}/lib_glueball
 
 echo ${PYTHONPATH}
 echo "Python setup run"
